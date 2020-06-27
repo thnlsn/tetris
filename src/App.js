65967c506c3dabbdd6b1d1f4daa4a5990f3369ae
Thomas Nelson <thnlsn@gmail.com>
2020-06-27 13:01:28 -0700

    function to create game grid

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './App.scss';
 
+import Tetris from './components/Tetris';
 import Grid from './components/grid/Grid';
 
 function App() {
-  const [score, setScore] = useState(0);
-
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <div className='App'>
-      <h3>
-        Score: <span className='score'>{score}</span>
-      </h3>
-      <button>Start/Pause</button>
-
-      <Grid />
+      <Tetris />
     </div>
   );
 }
---
+++ src/gameHelpers.js
@@ -0,0 +1,13 @@
+export const STAGE_WIDTH = 12;
+export const STAGE_HEIGHT = 20;
+
+// Multidimensional array to represent the grid
+export const createStage = () =>
+  // Creating an array from an array that is equal to 20 empty indexes (imagine a vertical array representing stage height)
+  Array.from(
+    Array(STAGE_HEIGHT),
+    () =>
+      // For every empty index in the 20 tall array, fill it with 12 indexes of [0, 'clear'] (imagine these as a usual horizontal array)
+      new Array(STAGE_WIDTH).fill([0, 'clear'])
+    // Together these represent a 20 tall and 12 wide grid for our game stage
+  );
