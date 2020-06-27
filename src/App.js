a696141669a0b72e844cdef834fe3c1bdd42064b
Thomas Nelson <thnlsn@gmail.com>
2020-06-26 23:57:26 -0700

    listed out tetrimino shapes in arrays

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.scss';
 import Grid from './components/grid/Grid';
 
 function App() {
+  const [score, setScore] = useState(0);
+
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <div className='App'>
       <h3>
-        Score: <span className='score'>0</span>
+        Score: <span className='score'>{score}</span>
       </h3>
       <button>Start/Pause</button>
 
--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -6,9 +6,44 @@ const Grid = () => {
 
   const width = 10;
 
+  // Tetrominoes
+  const tetrominoL = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
+
+  const tetrominoZ = [
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+  ];
+
+  const tetrominoT = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+
+  const tetrominoO = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+  const tetrominoI = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
+
   let grid = [];
   for (let i = 0; i < 200; i++) {
-    grid.push(<div className='grid__unit'></div>);
+    grid.push(<div key={i} className='grid__unit'></div>);
   }
 
   useEffect(() => {
@@ -17,6 +52,7 @@ const Grid = () => {
       squares.push(gridRef.current.children[i]);
     }
   }, []);
+
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <div className='grid' ref={gridRef}>
