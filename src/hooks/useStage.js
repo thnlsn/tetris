97ee1ce02e3ad8cfe8b765823ff2a2dda601ed17
Thomas Nelson <thnlsn@gmail.com>
2020-07-05 23:50:17 -0700

    useStage bug fixed

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,17 +3,26 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
-
+  /* 
   useEffect(() => {
-    const updateStage = (prevStage) => {
-      // First clear stage from previous render
+    const updateStage = prevStage => {
+      // First clear stage from previous render by removing everything that should not be there
       const newStage = prevStage.map((row) =>
         row.map((cell) => (cell[1] === 'clear' ? [0, 'clear'] : cell))
       );
+
+      // Then draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) = {
+          if (value !== 0) {
+            
+          }
+        })
+      })
     };
 
     setStage((prev) => updateStage(prev));
-  }, []);
+  }, []); */
 
   // Returning needed values in an array
   return [stage, setStage];
