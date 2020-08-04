9fd0376931a6c61dddaae72786bbb9a825c5b938
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-08-03 18:31:49 -0700

    collision detection function

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers';
+import { createStage, checkCollision } from '../gameHelpers';
 
 // Styled Components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
@@ -15,88 +15,88 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
-  // State
-  const [dropTime, setDropTime] = useState(null);
-  const [gameOver, setGameOver] = useState(false);
+    // State
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
 
-  // Custom Hooks, destructured
-  // player is the original state of player (which is technically at the top left of grid, but value 0 which is nothing)
-  // updatePlayerPos invokes setPlayer and takes it's previous properties and plus or minuses x/y values depending on keypress
-  // resetPlayer is what gives us out first real tetromino and centers it on the grid for it's initial placement
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+    // Custom Hooks, destructured
+    // player is the original state of player (which is technically at the top left of grid, but value 0 which is nothing)
+    // updatePlayerPos invokes setPlayer and takes it's previous properties and plus or minuses x/y values depending on keypress
+    // resetPlayer is what gives us out first real tetromino and centers it on the grid for it's initial placement
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
 
-  // stage is the inital stage built by createStage() in gameHelpers.js
-  const [stage, setStage] = useStage(player, resetPlayer); // Passing in player because we already have access to it
+    // stage is the inital stage built by createStage() in gameHelpers.js
+    const [stage, setStage] = useStage(player, resetPlayer); // Passing in player because we already have access to it
 
-  // Console.log whenever component re-renders
-  console.log('re-render');
+    // Console.log whenever component re-renders
+    console.log('re-render');
 
-  // Player movement -- Left/Right
-  const movePlayer = (direction) => {
-    updatePlayerPos({ x: direction, y: 0, collided: false });
-  };
+    // Player movement -- Left/Right
+    const movePlayer = (direction) => {
+        updatePlayerPos({ x: direction, y: 0, collided: false });
+    };
 
-  const startGame = () => {
-    // Reset everything
-    setStage(createStage()); // Creates blank stage
-    resetPlayer(); // Sets player to the middle of the grid
-  };
+    const startGame = () => {
+        // Reset everything
+        setStage(createStage()); // Creates blank stage
+        resetPlayer(); // Sets player to the middle of the grid
+    };
 
-  // Player movement -- Down
-  const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false });
-  };
+    // Player movement -- Down
+    const drop = () => {
+        updatePlayerPos({ x: 0, y: 1, collided: false });
+    };
 
-  const dropPlayer = () => {
-    drop();
-  };
+    const dropPlayer = () => {
+        drop();
+    };
 
-  // Move function using the key pressed (destructured) as parameter
-  const move = ({ keyCode }) => {
-    console.log(keyCode);
-    if (!gameOver) {
-      if (keyCode === 37) {
-        // Left
-        movePlayer(-1);
-      } else if (keyCode === 39) {
-        // Right
-        movePlayer(1);
-      } else if (keyCode === 40) {
-        // Down
-        dropPlayer();
-      } else if (keyCode === 38) {
-        // Up
-        // Rotate player tetromino
-      }
-    }
-  };
+    // Move function using the key pressed (destructured) as parameter
+    const move = ({ keyCode }) => {
+        console.log(keyCode);
+        if (!gameOver) {
+            if (keyCode === 37) {
+                // Left
+                movePlayer(-1);
+            } else if (keyCode === 39) {
+                // Right
+                movePlayer(1);
+            } else if (keyCode === 40) {
+                // Down
+                dropPlayer();
+            } else if (keyCode === 38) {
+                // Up
+                // Rotate player tetromino
+            }
+        }
+    };
 
-  return (
-    <StyledTetrisWrapper
-      role='button'
-      tabIndex='0'
-      onKeyDown={(event) => move(event)}
-    >
-      <StyledTetris>
-        {/* We invoke createStage() in the useStage hook as inital value for stage (this is the grid) */}
-        <Stage stage={stage} />
-        <aside>
-          {/* If gameOver is true, display a Display component with gameOver value so the styled component will set text to red */}
-          {gameOver ? (
-            <Display gameOver={gameOver} text='Game Over'></Display>
-          ) : (
-            /* Otherwise, if false, then show displays of the current game information */
-            <div>
-              <Display text='Score' />
-              <Display text='Rows' />
-              <Display text='Level' />
-            </div>
-          )}
-          <StartButton callback={startGame} />
-        </aside>
-      </StyledTetris>
-    </StyledTetrisWrapper>
-  );
+    return (
+        <StyledTetrisWrapper
+            role='button'
+            tabIndex='0'
+            onKeyDown={(event) => move(event)}
+        >
+            <StyledTetris>
+                {/* We invoke createStage() in the useStage hook as inital value for stage (this is the grid) */}
+                <Stage stage={stage} />
+                <aside>
+                    {/* If gameOver is true, display a Display component with gameOver value so the styled component will set text to red */}
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text='Game Over'></Display>
+                    ) : (
+                        /* Otherwise, if false, then show displays of the current game information */
+                        <div>
+                            <Display text='Score' />
+                            <Display text='Rows' />
+                            <Display text='Level' />
+                        </div>
+                    )}
+                    <StartButton callback={startGame} />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
+    );
 };
 
 export default Tetris;
--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -3,25 +3,39 @@ export const STAGE_HEIGHT = 20;
 
 // Multidimensional array to represent the grid
 export const createStage = () =>
-  // Creating an array from an array that is equal to 20 empty indexes (imagine a vertical array representing stage height)
-  Array.from(
-    Array(STAGE_HEIGHT),
-    () =>
-      // For every empty index in the 20 tall array, fill it with 12 indexes of [0, 'clear'] (imagine these as a usual horizontal array)
-      new Array(STAGE_WIDTH).fill([0, 'clear']) // 0 represents whether a tetromino block is in the cell and clear will remove it if it leaves
-    // Together these represent a 20 tall and 12 wide grid for our game stage
-  );
+    // Creating an array from an array that is equal to 20 empty indexes (imagine a vertical array representing stage height)
+    Array.from(
+        Array(STAGE_HEIGHT),
+        () =>
+            // For every empty index in the 20 tall array, fill it with 12 indexes of [0, 'clear'] (imagine these as a usual horizontal array)
+            new Array(STAGE_WIDTH).fill([0, 'clear']) // 0 represents whether a tetromino block is in the cell and clear will remove it if it leaves
+        // Together these represent a 20 tall and 12 wide grid for our game stage
+    );
 
+// change x and y to moveX and moveY because x and y are needed in the loops
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
-  for (let y = 0; y < player.tetromino.length; y += 1) {
-    for (let x = 0; x < player.tetromino[0].length; x += 1) {
-      // 1. Check if there is in fact a tetromino block (as in IJLOSTZ rather than 0)
-      // A letter will coerce to true, but 0 will coerce to false
-      if (player.tetromino[y][x]) {
-        // 2. Check that it is inside the grid height (y), as in not going below the 20 tall grid
-        // 3. Check that the move is within the grid width (x), as in the 12 wide grid
-        // 4. Check that the cell we're moving to isn't set to clear
-      }
+    for (let y = 0; y < player.tetromino.length; y += 1) {
+        for (let x = 0; x < player.tetromino[0].length; x += 1) {
+            // 1. Check if there is in fact a tetromino block (as in IJLOSTZ rather than 0)
+            // A letter will coerce to true, but 0 will coerce to false
+            if (player.tetromino[y][x] !== 0) {
+                if (
+                    // 2. Check that it is inside the grid height (y), as in not going below the 20 tall grid
+                    // Chained short-circuit (checks if first condition is true then stop, if not check second and stop, and so on)
+                    !stage[y + player.pos.y + moveY] || // Move to next condition if this is not true
+                    // 3. Check that the move is within the grid width (x), as in the 12 wide grid
+                    !stage[y + player.pos.y + moveY][
+                        x + player.pos.x + moveX
+                    ] || // Move to next condition if this is not true
+                    // 4. Check that the cell we're moving to isn't set to clear (if clear, then no collision)
+                    stage[y + player.pos.y + moveY][
+                        x + player.pos.x + moveX
+                    ][1] !== 'clear'
+                    // So if the cell we are moving to IS set to clear, then collision is true
+                ) {
+                    return true;
+                }
+            }
+        }
     }
-  }
 };
